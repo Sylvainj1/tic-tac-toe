9a2898939e614aeb73023056d5ab451929d870fa
sylvainjiang <sylvainjiang1@gmail.com>
2020-11-04 17:11:03 +0100

    add tic tac toe game

---
+++ test.scala
@@ -0,0 +1,22 @@
+import Array._
+import util.control.Breaks._
+
+object MyProjectMain {
+  def main(args: Array[String]): Unit = {
+   
+    println("Joueur X : entrer la ligne")
+
+    try{
+      val x=scala.io.StdIn.readInt()
+      if(x.isInstanceOf[Int]==true){
+        println(x)
+      }else{
+        println("error")
+      }
+    }catch{
+      case e: Throwable => print("error 2")
+    }
+         
+  }
+
+}
